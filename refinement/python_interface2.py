21ca3f473744a94f60a2a59536d33d05b209a3db
Anoop Rehman <93463679+anoop-rehman@users.noreply.github.com>
2024-02-21 04:46:54 -0500

    What on earth is GrabP1 bruh Im sorry but I have to pivot for now

--- refinement/python_interface2.py
+++ refinement/python_interface2.py
@@ -1,5 +1,45 @@
+# from mlagents_envs.environment import UnityEnvironment
+# from mlagents_envs.base_env import ActionTuple
+
+# # Path to your Unity executable
+# env_path = "/Users/anooprehman/Documents/uoft/extracurricular/design_teams/utmist2/bodiesTEST3/unity_projects/engine 2/Builds/v3testBuild2_bigVec.app"
+
+# try:
+#     # Launch the environment
+#     env = UnityEnvironment(file_name=env_path)
+
+#     # Reset the environment
+#     env.reset()
+#     behavior_name = list(env.behavior_specs)[0]
+#     spec = env.behavior_specs[behavior_name]
+
+#     decision_steps, terminal_steps = env.get_steps(behavior_name)
+
+#     # # Example of taking an action
+#     # action = spec.create_empty_action(len(decision_steps))
+#     # action_tuple = ActionTuple(continuous=action)
+#     # env.set_actions(behavior_name, action_tuple)
+
+#     env.step()  # Advance the environment by one step
+
+#     # Getting the Observation Vector
+#     # decision_steps, terminal_steps = env.get_steps(behavior_name)
+#     # for obs in decision_steps.obs:
+#     #     print(obs)  # This will print the observation vectors for each agent
+
+#     decision_steps, terminal_steps = env.get_steps(behavior_name)
+#     for agent_id in decision_steps:
+#         obs = decision_steps[agent_id].obs
+#         print(obs)  # This should print the observation vector for each agent
+
+#     # Don't forget to close the environment
+#     env.close()
+# except Exception as e:
+#     print(e)
+
+
+
 from mlagents_envs.environment import UnityEnvironment
-from mlagents_envs.base_env import ActionTuple
 
 # Path to your Unity executable
 env_path = "/Users/anooprehman/Documents/uoft/extracurricular/design_teams/utmist2/bodiesTEST3/unity_projects/engine 2/Builds/v3testBuild2_bigVec.app"
@@ -13,26 +53,22 @@ try:
     behavior_name = list(env.behavior_specs)[0]
     spec = env.behavior_specs[behavior_name]
 
-    decision_steps, terminal_steps = env.get_steps(behavior_name)
+    # Step the environment
+    env.step()
 
-    # # Example of taking an action
-    # action = spec.create_empty_action(len(decision_steps))
-    # action_tuple = ActionTuple(continuous=action)
-    # env.set_actions(behavior_name, action_tuple)
-
-    env.step()  # Advance the environment by one step
-
-    # Getting the Observation Vector
-    # decision_steps, terminal_steps = env.get_steps(behavior_name)
-    # for obs in decision_steps.obs:
-    #     print(obs)  # This will print the observation vectors for each agent
-
+    # Getting the Observation Vector for each agent
     decision_steps, terminal_steps = env.get_steps(behavior_name)
     for agent_id in decision_steps:
-        obs = decision_steps[agent_id].obs
-        print(obs)  # This should print the observation vector for each agent
+        # Accessing observations for this agent
+        observations = decision_steps[agent_id].obs
+
+        # Print or process the observations
+        # Note: If you have multiple observation types (vector, visual), they will be in separate elements of the 'observations' list
+        print(f"Observations for agent {agent_id}:")
+        for obs_idx, obs in enumerate(observations):
+            print(f"  Observation space {obs_idx}: {obs}")
 
     # Don't forget to close the environment
     env.close()
 except Exception as e:
-    print(e)
+    print(f"An error occurred: {e}")
